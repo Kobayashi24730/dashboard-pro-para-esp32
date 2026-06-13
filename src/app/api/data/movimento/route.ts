9f64f67f189d84bb3931d4578aa7793b01615940
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-06-13 12:22:38 -0300

    Correções e desenvolvimento da rota do wesp32 para integração com a database

--- src/app/api/data/movimento/route.ts
+++ src/app/api/data/movimento/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import sqlite3 from "sqlite3";
+import Database from "better-sqlite3";
 import path from "path";
 
 const pathDB = path.join(process.cwd(), 'backend', 'indentifier.sqlite');
-const db = new sqlite3.Database(pathDB);
+const db = new Database(pathDB);
 
 let ultimoEstado = {
     device_id: "ESP32_PIR_01",
@@ -13,19 +13,12 @@ let ultimoEstado = {
 
 export async function POST(request: NextRequest){
     ultimoEstado = await request.json();
-    db.run(`INSERT INTO sensor_data (device_id, sensor, estado) VALUES (?, ?, ?)`, [ultimoEstado.device_id, ultimoEstado.sensor, ultimoEstado.estado ? 1 : 0]);
+    db.prepare(`INSERT INTO sensor_data (device_id, sensor, estado) VALUES (?, ?, ?)`).run([ultimoEstado.device_id, ultimoEstado.sensor, ultimoEstado.estado ? 1 : 0]);
     return NextResponse.json({ success: true });
 }
 
 export async function GET() {
     const rows = await new Promise((resolve, reject) =>  {
-        db.all(`SELECT * FROM sensor_data ORDER BY created_at DESC LIMIT 50`, [], (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
-    });
+        db.prepare(`SELECT * FROM sensor_data ORDER BY created_at DESC LIMIT 50`).all();
     return NextResponse.json(rows);
 }
