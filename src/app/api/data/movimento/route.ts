c1a7296ee9854c1d313dd4fd2c8a947903d41f7c
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-06-12 22:55:54 -0300

    Correções e desenvolvimento da rota para o wesp32

--- src/app/api/data/movimento/route.ts
+++ src/app/api/data/movimento/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET() {
+    return NextResponse.json({ success: true, message: "API funcionando" });
+}
 export async function POST(request: NextRequest){
     const data = await request.json();
     console.log(data);
