6e60ca9a6c5e0fa4fb27a16b14efdfa6e58e9f23
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-06-20 12:40:31 -0300

    Correções e ajuste na api

--- src/backend/data/useTextValues.ts
+++ src/backend/data/useTextValues.ts
@@ -7,34 +7,18 @@ export interface TextFormatValues {
 }
 export async function getValues() {
     try {
-        const baseUrl =
-            process.env.NEXT_PUBLIC_APP_URL ||
-            "https://dashboard-pro-para-esp32.onrender.com";
-
-        const response = await fetch(
-            `${baseUrl}/api/data/movimento`,
-            {
-                cache: "no-store"
-            }
-        );
-
+        const response = await fetch('/api/data/movimento', {
+            method: "GET",
+            cache: "no-store"
+        });
         if (!response.ok) {
             throw new Error(`HTTP ${response.status}`);
         }
-
         return await response.json();
 
     } catch (error) {
         console.error("Erro ao buscar dados:", error);
-
-        return [
-            {
-                device_id: "TESTE",
-                sensor: "PIR",
-                estado: false,
-                valor: 0
-            }
-        ];
+        return json;
     }
 }
 
